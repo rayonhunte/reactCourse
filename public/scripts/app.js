e915612765e3cabea36505900c603cb0d1d9e824
Rayon Hunte <rayon.hunte@gmail.com>
2017-11-09 11:50:17 -0400

    update

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,3 +3,15 @@
 var nameLet = 'Jen';
 nameLet = 'Julie';
 console.log('nameLet', nameLet);
+
+//Block Scoping
+
+var fullName = 'Jen Mead';
+var firstName = void 0;
+
+if (fullName) {
+  firstName = fullName.split(' ')[0];
+  console.log(firstName);
+}
+
+console.log(firstName);
--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,3 +1,16 @@
 let nameLet = 'Jen';
 nameLet = 'Julie'
 console.log('nameLet', nameLet);
+
+
+//Block Scoping
+
+let fullName = 'Jen Mead';
+let firstName;
+
+if (fullName) {
+  firstName = fullName.split(' ')[0]
+  console.log(firstName);
+}
+
+console.log(firstName);
