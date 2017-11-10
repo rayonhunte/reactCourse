f4434ec9ed252f9f3064899d40a7f6422d7a08c5
Rayon Hunte <rayon.hunte@gmail.com>
2017-11-10 04:39:49 -0400

    options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,46 +5,83 @@
 var app = {
   title: "My Indecision",
   subTitle: "react is cool",
-  options: ['One', 'Two']
+  options: []
+};
 
-  //JSX - JavaScript XML 
-};var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    app.title
-  ),
-  app.subTitle && React.createElement(
-    "p",
-    null,
-    app.subTitle
-  ),
-  app.options ? React.createElement(
-    "p",
-    null,
-    "Here are your options"
-  ) : React.createElement(
-    "p",
-    null,
-    "No Options"
-  ),
-  React.createElement(
-    "ol",
-    null,
-    React.createElement(
-      "li",
-      null,
-      "1"
-    ),
-    React.createElement(
-      "li",
-      null,
-      "2"
-    )
-  )
-);
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+  if (option) {
+    console.log(option);
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderApp();
+  }
+};
+
+var reset = function reset() {
+  app.options = [];
+  renderApp();
+};
 
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+var renderApp = function renderApp() {
+  //JSX - JavaScript XML 
+  var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      app.title
+    ),
+    app.subTitle && React.createElement(
+      "p",
+      null,
+      app.subTitle
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length > 0 ? "Here Are your options" : 'No Options'
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      "button",
+      { onClick: reset },
+      "RemoveAll"
+    ),
+    React.createElement(
+      "ol",
+      null,
+      app.options.map(function (option, index) {
+        console.log(index);
+        return React.createElement(
+          "li",
+          { key: index },
+          "  ",
+          option,
+          "  "
+        );
+      })
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: onFormSubmit },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        { type: "submit" },
+        "Add Option"
+      )
+    )
+  );
+  ReactDOM.render(template, appRoot);
+};
+
+renderApp();
--- src/app.js
+++ src/app.js
@@ -3,23 +3,54 @@
 const app = {
   title: "My Indecision",
   subTitle: "react is cool",
-  options: ['One', 'Two']
+  options: []
+}
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  if (option) {
+    console.log(option);
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderApp();
+  }
+}
+
+const reset = () => {
+  app.options = [];
+  renderApp();
 }
 
 
 
-//JSX - JavaScript XML 
-var template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subTitle && <p>{app.subTitle}</p>}
-    {app.options ? <p>Here are your options</p> : <p>No Options</p>}
-    <ol>
-      <li>1</li>
-      <li>2</li>
-    </ol>
-  </div>);
-
-
 const appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+
+const renderApp = () => {
+  //JSX - JavaScript XML 
+  var template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subTitle && <p>{app.subTitle}</p>}
+      <p>{app.options.length > 0 ? `Here Are your options`: 'No Options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={reset}>RemoveAll</button>
+      <ol>
+      {
+        app.options.map((option, index)=>{
+          console.log(index);  
+          return <li key={index}>  {option}  </li>
+        })
+      }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button type="submit">Add Option</button>
+      </form>
+    </div>);
+    ReactDOM.render(template, appRoot);
+}
+
+
+renderApp();
